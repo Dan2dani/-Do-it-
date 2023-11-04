b067d3caf6058ccccc6cfc5eba84871d8d758b76
82106 <eldms0406@naver.com>
2023-11-04 13:18:55 +0900

    자료구조: P11659_구간합구하기(정답코드)

--- src/자료구조/P11659_구간합구하기.java
+++ src/자료구조/P11659_구간합구하기.java
@@ -1,31 +1,28 @@
 package 자료구조;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class P11659_구간합구하기 {
-    public class Main {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-
-            int N = scanner.nextInt();
-            int M = scanner.nextInt();
-            int sums = 0;
-            long[] totalSums = new long[N];
-
-            for (int i = 0; i < totalSums.length; i++) {
-                sums += scanner.nextInt();
-                totalSums[i] = sums;
-            }
-
-            for (int i = 0; i < M; i++) {
-                int lower = scanner.nextInt();
-                int upper = scanner.nextInt();
-
-                if (lower == 1)
-                    System.out.println(totalSums[upper - 1]);
-                else
-                    System.out.println(totalSums[upper - 1] - totalSums[lower - 2]);
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader
+                = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer
+                = new StringTokenizer(bufferedReader.readLine());
+        int suNo = Integer.parseInt(stringTokenizer.nextToken());
+        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+        long[] S = new long[suNo + 1];
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 1; i <= suNo; i++) {
+            S[i] = S[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
+        }
+        for (int q = 0; q < quizNo; q++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(S[j] - S[i - 1]);
         }
     }
 }
